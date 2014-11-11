f7b783b449c9be9f1de070d09f3c02724748f55f
Linda Linsefors <linda@linda-laptop>
2014-11-11 20:57:03 +0100

    first commit

---
+++ plots/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from plots.models import *
+
+admin.site.register(Group)
+admin.site.register(Character)
+admin.site.register(Plot_line)
+admin.site.register(Plot)
---
+++ plots/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class BasicModel(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
+class Group(BasicModel):
+    group_description = models.TextField(blank=True, default='')
+
+
+class Character(BasicModel):
+    character_description = models.TextField(blank=True, default='')
+    comments_from_God = models.TextField(blank=True, default='')
+
+    group = models.ForeignKey(  Group,  
+                                null=True, 
+                                on_delete=models.SET_NULL )
+
+
+class Plot_line(BasicModel):
+    summery = models.TextField()
+
+
+class Plot(BasicModel):
+    group = models.ForeignKey(  Group,  
+                                null=True, 
+                                on_delete=models.SET_NULL )
+
+    character = models.ForeignKey(  Character, 
+                                    null=True, 
+                                    on_delete=models.SET_NULL )
+
+    plot_line = models.ForeignKey(  Plot_line, 
+                                    null=True, 
+                                    on_delete=models.SET_NULL )
+
+    plot = models.TextField(blank=True, default='')
+
+
+    def group_plot(self):
+        return not self.group
+    group_plot.boolean = True
+
+    def del_av_plot_line(self):
+        return not self.plot_line
+    group_plot.boolean = True

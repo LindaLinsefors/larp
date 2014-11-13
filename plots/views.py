abf5f3d2471174d3d5d2b429fa817dd5a7195295
Linda Linsefors <linda@linda-laptop>
2014-11-13 22:43:19 +0100

    Bug fix

--- plots/views.py
+++ plots/views.py
@@ -7,8 +7,12 @@ from plots.models import Group
 
 
 def groups(request):
-    open_groups = Group.objects.filter(is_open=True),
-    closed_groups = Group.objects.filter(is_open=True) 
+    open_groups = Group.objects.filter(is_open=True)
+    if open_groups.count() == 0: 
+        open_groups = False
+    closed_groups = Group.objects.filter(is_open=False) 
+    if closed_groups.count() == 0: 
+        closed_groups = False
     return render(request, 
                   'plots/groups.html', 
                   {'open_groups': open_groups, 
@@ -16,10 +20,8 @@ def groups(request):
 
 
 
-#def group(request, group_id):
-#    group = get_object_or_404(Group, pk=group_id)
-#    return render(request, 'plots/grupe.html', {'group': groups} )
+def group(request, group_id):
+    group = get_object_or_404(Group, pk=group_id)
+    return render(request, 'plots/group.html', {'group': group} )
 
 
-def group(pryl):
-    return HttpResponse(repr(pryl))

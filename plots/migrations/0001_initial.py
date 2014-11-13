ac13d5a7ce8bbbf71207d467f844aaa77ed1e437
Linda Linsefors <linda@linda-laptop>
2014-11-13 13:17:22 +0100

    It works

--- plots/migrations/0001_initial.py
+++ plots/migrations/0001_initial.py
@@ -15,8 +15,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50)),
+                ('character_concept', models.CharField(max_length=50)),
+                ('plot_is_finished', models.BooleanField(default=False)),
                 ('character_description', models.TextField(default=b'', blank=True)),
-                ('comments_from_God', models.TextField(default=b'', blank=True)),
+                ('comments_from_God', models.TextField(default=b'', help_text=b'Comments on the character from GM to the player.', blank=True)),
             ],
             options={
                 'abstract': False,
@@ -29,6 +31,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50)),
                 ('group_description', models.TextField(default=b'', blank=True)),
+                ('plot_is_finished', models.BooleanField(default=False)),
             ],
             options={
                 'abstract': False,
@@ -40,9 +43,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50)),
+                ('plot_is_finished', models.BooleanField(default=False)),
                 ('plot', models.TextField(default=b'', blank=True)),
-                ('characters', models.ManyToManyField(to='plots.Character', null=True)),
-                ('groups', models.ManyToManyField(to='plots.Group', null=True)),
+                ('characters', models.ManyToManyField(to='plots.Character', null=True, blank=True)),
+                ('groups', models.ManyToManyField(to='plots.Group', null=True, blank=True)),
             ],
             options={
                 'abstract': False,
@@ -54,6 +58,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50)),
+                ('plot_is_finished', models.BooleanField(default=False)),
                 ('summery', models.TextField(default=b'', blank=True)),
             ],
             options={
@@ -64,13 +69,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='plot',
             name='plot_lines',
-            field=models.ManyToManyField(to='plots.Plot_line', null=True),
+            field=models.ManyToManyField(to='plots.Plot_line', null=True, blank=True),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='character',
             name='groups',
-            field=models.ManyToManyField(to='plots.Group', null=True),
+            field=models.ManyToManyField(to='plots.Group', null=True, blank=True),
             preserve_default=True,
         ),
     ]

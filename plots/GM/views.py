ec95e9b95d6b940a6e370fb291ebfc8404462035
Linda Linsefors <linda@linda-laptop>
2014-12-10 23:40:26 +0100

    built PlotPartForm

--- plots/GM/views.py
+++ plots/GM/views.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 from django.core.exceptions import PermissionDenied
 from django import forms
 
-from plots.models import PlotThread
+from plots.models import PlotThread, PlotPart, PlotPice
 
 
 def index(request): 
@@ -22,6 +22,24 @@ class PlotThreadForm(forms.ModelForm):
                     'summery', 
                     'plot_is_finished'     ]
 
+class PlotPartForm(forms.ModelForm):
+    class Meta:
+        model = PlotPart
+        fields = [ 'rank' ]
+
+    plot_pice = forms.CharField(widget = forms.Textarea)
+
+    def __init__(self, *args, **kw):
+        forms.ModelForm.__init__(self, *args, **kw)
+        if kw.has_key('instance'):
+            self.fields['plot_pice'].initial = self.instance.plot_pice.plot_pice
+
+    def save(self):
+        self.is_valid()
+        forms.ModelForm.save(self)
+        self.instance.plot_pice.plot_pice = self.cleaned_data['plot_pice']
+        self.instance.plot_pice.plot_pice.save()
+
 
 
 def plot_thread(request, id): 
@@ -32,9 +50,12 @@ def plot_thread(request, id):
         if plot_thread_form.is_valid():
             plot_thread_form.save()
     else: 
-        plot_thread_form = PlotThreadForm(instance=plot_thread)     
+        plot_thread_form = PlotThreadForm(instance=plot_thread)    
+
+    plot_part_forms = [ PlotPartForm(instance=plot_part) for plot_part in plot_thread.plotpart_set.all()]
 
     return render(request, 'plots/GM_plot_thread.html',
            {'plot_thread_form': plot_thread_form,
-            'plot_thread': plot_thread }   )       
+            'plot_thread': plot_thread ,
+            'plot_part_forms': plot_part_forms}   )       
 

ed0136fb3ef6332f0b58e5fdea3caacab4288987
Linda Linsefors <linda@linda-laptop>
2014-12-10 22:41:47 +0100

    more GM interface

--- plots/GM/views.py
+++ plots/GM/views.py
@@ -13,5 +13,28 @@ def index(request):
             { 'plot_threads': PlotThread.objects.all() }    )
 
 
+
+
+class PlotThreadForm(forms.ModelForm):
+    class Meta:
+        model = PlotThread
+        fields = [  'name', 
+                    'summery', 
+                    'plot_is_finished'     ]
+
+
+
 def plot_thread(request, id): 
-    return render(request, 'plots/GM_plot_thread.html')
+    plot_thread = get_object_or_404(PlotThread, pk=id)
+
+    if request.method == 'POST':
+        plot_thread_form = PlotThreadForm(request.POST, instance=plot_thread)
+        if plot_thread_form.is_valid():
+            plot_thread_form.save()
+    else: 
+        plot_thread_form = PlotThreadForm(instance=plot_thread)     
+
+    return render(request, 'plots/GM_plot_thread.html',
+           {'plot_thread_form': plot_thread_form,
+            'plot_thread': plot_thread }   )       
+

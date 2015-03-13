a3cc85e6611f1af9bff7e120d6d60bd73fae223b
Linda Linsefors <linda@linda-laptop>
2015-03-13 15:31:55 +0100

    can add pages

--- larp/urls.py
+++ larp/urls.py
@@ -18,9 +18,11 @@ urlpatterns = patterns('',
     url(r'^GM/', include('plots.GM.urls', namespace='GM')),
 
     url(r'^$', views.home, name='home'),
+    url(r'^new', views.new_top_page, name='new_top_page'),
     url(r'^page/(?P<id>\d+)/$', views.page, name='page'),
-    url(r'^page/(?P<id>\d+)/edit/$', views.edit_page, name='edit_page'),
-    url(r'^page/(?P<id>\d+)/edit/delete$', views.delete_page, name='delete_page'),
+    url(r'^page/(?P<id>\d+)/new$', views.new_subpage, name='new_subpage'),
+    url(r'^page/(?P<id>\d+)/edit$', views.edit_page, name='edit_page'),
+    url(r'^page/(?P<id>\d+)/delete$', views.delete_page, name='delete_page'),
 
     #url(r'^/', include('pages.urls', namespace='pages')),
     
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 from pages.models import Page
 
 import floppyforms.__future__  as forms
@@ -7,7 +10,10 @@ import floppyforms.__future__  as forms
 class PageForm(forms.ModelForm):
     class Meta:
         model = Page
-
+        fields = [  'title',
+                    'top_page',
+                    'sort_under',
+                    'html',         ]
 
 
 def nav_list():
@@ -23,7 +29,7 @@ def home(request):
     except(Page.MultipleObjectsReturned):
         return render(  request, 'pages/page.html',
                {'nav_list': nav_list(),
-                'error_message': '''
+                'my_error_message': '''
 You have more than one page titled "home".
 Ahrgh, the confusion! I can't stand it. Fix this problem now!!
 '''}   ) 
@@ -48,15 +54,17 @@ def edit_page(request, id):
         return render( request, 'pages/edit_page.html',
                        {'nav_list': nav_list(),
                         'page_form': PageForm(instance=page),
-                        'id': id,                               })
+                        'id': id,   
+                        'delete':'delete',                       })
 
     page_form = PageForm(request.POST, instance=page)
     if not page_form.is_valid():
         return render( request, 'pages/edit_page.html',
-                       {'error_message': 'What?! Form is not valid? This should not happen.',
+                       {'my_error_message': 'What?! Form is not valid? This should not happen.',
                         'nav_list': nav_list(),
                         'page_form': page_form,
-                        'id': id,                               })
+                        'id': id,
+                        'delete':'delete',                       })
 
     page_form.save()
     return HttpResponseRedirect( reverse('page', args=(id,)) ) 
@@ -66,4 +74,40 @@ def delete_page(request, id):
     get_object_or_404(Page, pk=id).delete()
     return HttpResponseRedirect( reverse('home') ) 
 
+def new_top_page(request):
+    if request.method != 'POST':
+        return render( request, 'pages/edit_page.html',
+                       {'nav_list': nav_list(),
+                        'page_form': PageForm(initial={'top_page':True}), })
+
+    page_form = PageForm(request.POST)
+    if not page_form.is_valid():
+        return render( request, 'pages/edit_page.html',
+                       {'my_error_message': 'What?! Form is not valid? This should not happen.',
+                        'nav_list': nav_list(),
+                        'page_form': page_form,      })
+
+    page_form.save()
+    return HttpResponseRedirect( reverse('page', args=(page_form.instance.id,)) ) 
+    
+
+def new_subpage(request, id):
+    if request.method != 'POST':
+        page = get_object_or_404(Page, pk=id)
+        return render( request, 'pages/edit_page.html',
+                       {'nav_list': nav_list(),
+                        'page_form': PageForm({ 'top_page': False,
+                                                'sort_under': page,  }),
+                        'id': id                    })
+
+    page_form = PageForm(request.POST)
+    if not page_form.is_valid():
+        return render( request, 'pages/edit_page.html',
+                       {'my_error_message': 'What?! Form is not valid? This should not happen.',
+                        'nav_list': nav_list(),
+                        'page_form': page_form,
+                        'id': id                    })
+
+    page_form.save()
+    return HttpResponseRedirect( reverse('page', args=(page_form.instance.id,)) ) 
 
